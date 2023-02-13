b2c15bac4a78d0668178abc17e7ec1ea1fb8bf5c
rkarthikraj <rkarthykraj@gmail.com>
2023-02-13 22:03:35 +0530

    LC DP practise

---
+++ src/leetcode/dp/TribonacciNumber.java
@@ -0,0 +1,27 @@
+package leetcode.dp;
+
+class TribonacciNumber {
+    public static void main(String[] args) {
+        TribonacciNumber obj = new TribonacciNumber();
+        System.out.println(obj.tribonacci(4));
+        System.out.println(obj.tribonacci(0));
+        System.out.println(obj.tribonacci(37));
+    }
+
+    public int tribonacci(int n) {
+        if (n < 2) return n;
+        if (n == 2) return 1;
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+
+        //System.out.println(Arrays.toString(dp));
+        return dp[n];
+    }
+}
